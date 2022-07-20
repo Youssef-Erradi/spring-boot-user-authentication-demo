66d40c8420cc751f2470fafdf06d96ee658ca27f
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-07-20 08:05:00 +0100

    initial commit

---
+++ src/main/java/com/example/UserAuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationDemoApplication.class, args);
+	}
+
+}
