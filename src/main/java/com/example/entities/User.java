698088a3d198bf1daa4519baa9bfaf022c152e05
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-07-20 08:13:28 +0100

    add user entity

---
+++ src/main/java/com/example/entities/User.java
@@ -0,0 +1,86 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.example.enums.Role;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = -3471921864525437356L;
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 50)
+	private String fullName;
+	
+	@Column(unique = true, nullable = false, length = 50)
+	private String email;
+	
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private Role role = Role.USER;
+	
+	
+	public User() {}
+
+	public User(String fullName, String email, String password, Role role) {
+		this.fullName = fullName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [fullName=" + fullName + ", email=" + email + "]";
+	}
+	
+}
